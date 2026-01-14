42b3c554c391bf4c6c6707c520c83bbf2bc189a9
Kevin Solano <k.solano18@icloud.com>
2026-01-13 18:43:20 -0600

    main: Base Project

---
+++ HatchWorksChallenge/HatchWorksChallenge/HatchWorksChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HatchWorksChallengeApp.swift
+//  HatchWorksChallenge
+//
+//  Created by Kevin Solano Jiménez on 13/1/26.
+//
+
+import SwiftUI
+
+@main
+struct HatchWorksChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
